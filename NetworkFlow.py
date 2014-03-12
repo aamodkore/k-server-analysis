463e7cb08bae714bec96350ec110616ed664d0f5
Aamod Kore <aamodkore@gmail.com>
2014-03-13 02:45:03 +0530

    Documentation for Network Flow added, Work Function class added

--- NetworkFlow.py
+++ NetworkFlow.py
@@ -1,19 +1,26 @@
 import queue
 
+"""A large real number"""
 LRN = 999999
+"""An even larger infinity"""
 infinity = 9999999999999
+
 class Edge(object):
+	"""Class for directed edge in graph"""
 	def __init__(self, u, v, cp, cs):
 		self.source = u
 		self.sink = v  
 		self.capacity = cp
 		self.cost = cs
+		"""redge stores the reverse edge 
+		from the sink to source"""
 		self.redge = None
 	def __repr__(self):
 		return "%s->%s:%s:%s" % (self.source, self.sink, self.capacity, self.cost)
 
  
 class FlowNetwork(object):
+	"""Class for the graph of the network"""
 	def __init__(self):
 		self.adj = {}
 		self.distance = {}
@@ -41,6 +48,8 @@ class FlowNetwork(object):
 	def get_flow(self, edge) :
 		return self.flow[edge]
 
+	"""Adding an edge also adds its reverse edge.
+	Thus in all there may be 4 edges between 2 vertices."""
 	def add_edge(self, u, v, w=0, c=1):
 		if u == v:
 			raise ValueError("u == v")
@@ -53,6 +62,8 @@ class FlowNetwork(object):
 		self.flow[edge] = 0
 		self.flow[redge] = 0
 
+	"""Using Bellman-Ford Algorithm to calculate 
+				non-negative vertex potentials. """
 	def bellman_ford_potentials(self, source) :
 		for u in self.vertices :
 			self.distance[u] = infinity
@@ -70,6 +81,7 @@ class FlowNetwork(object):
 			if acc == 0 :
 				break
 
+	"""Calculate least cat path using Bellman-Ford algorithm"""
 	def bellman_ford_path(self, source, sink) :
 		predecessor = {}
 		for u in self.vertices :
@@ -99,6 +111,8 @@ class FlowNetwork(object):
 		return None
 
 
+	"""Reduce edge costs to non-negative values,without 
+				altering possible least-cost paths"""
 	def reduce_costs(self) :
 		for u in self.vertices :
 			for edge in self.adj[u] :
@@ -108,6 +122,9 @@ class FlowNetwork(object):
 					edge.cost = edge.cost + self.distance[u] - self.distance[v]
 					edge.redge.cost = 0
 
+	
+	"""Find least cost path using Djikstra's Algorithm 
+				(assumes non-negative edge costs)"""
 	def find_path(self, source, sink):
 		openList = set([])
 		predecessor = {}
@@ -141,6 +158,9 @@ class FlowNetwork(object):
 		return None
 
  
+	"""Find Minimum cost maximum flow using repeated edge relaxation
+				along least cost path, found using
+				Belmann-Ford algorithm"""
 	def max_flow(self, source, sink):
 		path = self.bellman_ford_path(source, sink)
 		while path != None:
@@ -153,6 +173,12 @@ class FlowNetwork(object):
 		return sum(self.flow[edge] for edge in self.get_edges(source))
 
 
+	"""Find Minimum cost maximum flow using repeated edge relaxation
+				along least cost path, found using
+				reduced edge costs and reduced vertex 
+				potentials found by Belmann-Ford algorithm
+		For more details refer:
+	http://community.topcoder.com/tc?module=Static&d1=tutorials&d2=minimumCostFlow2"""
 	def min_cost_max_flow(self, source, sink):
 		self.bellman_ford_potentials(source)
 		self.reduce_costs()
@@ -170,19 +196,29 @@ class FlowNetwork(object):
 
 
 class ServerSpace(object):
+	"""Stores the metric space for the servers and the
+				initial server positions"""
 	def __init__(self, m):
 		self.servers = []
 		self.metric = m
 	
+	"""Adds a server given the initial position of the server"""
 	def add_server(self, pos) :
 		self.servers.append(pos)
 
+	"""For each member in the list provided, adds a server at the
+				corrosponding initial position"""
 	def add_servers(self,poslist) :
 		self.servers += poslist
 
+	"""Computes offline the optimal cost for processing a given
+				sequence of requests.
+		For complete algorithm refer :
+		M Chrobak, H Karloff, T Payne, S Vishwanathan: New Results on Server Problems"""
 	def process_requests(self, requests) :
-		process = [] 
 		
+		"""Maps(dictionary maps) to store nodes in the
+			graph corresponding to servers and requests"""
 		snode = {}
 		rnode = {}
 		ventity = {}
@@ -190,8 +226,9 @@ class ServerSpace(object):
 		ns = len(self.servers)
 		nr = len(requests)
 		n = ns + 2*nr + 1
-		graph = FlowNetwork()
 		
+		"""Create the corresponding flow network graph"""
+		graph = FlowNetwork()
 		graph.add_vertex(0)
 		ventity[0] = 0
 		graph.add_vertex(n)
@@ -216,72 +253,56 @@ class ServerSpace(object):
 			ventity[v] = s
 			snode[s] = v
 			v += 1
+		
+		"""Calculate min-cost max flow in the graph"""
 		k = graph.min_cost_max_flow(0,n)
 		if k != ns :
 			print(graph)
 			raise RuntimeError("Something went wrong: k=%s, ns=%s " % (k,ns))
 
+		"""Evaluate total cost and sequence in which the 
+				servers process the requests"""
+		process = [] 
+		totalCost = 0
 		for s in range(ns) :
 			v = snode[s] 
 			rlist = []
 			while v != n :
 				if v > 0 and v <= 2*nr and v%2:
-					rlist.append(ventity[v])
+					r = ventity[v]
+					if len(rlist)==0 :
+						totalCost += self.metric(self.servers[s], requests[r])
+					else :
+						totalCost += self.metric(requests[rlist[-1]], requests[r])
+					rlist.append(r)
 				for edge in graph.get_edges(v) :
 					if graph.get_flow(edge) > 0 :
 						v = edge.sink
 						break
 			process.append((s, rlist))
 
-		return process
+		return (totalCost, process)
 
-if False :
-	g = FlowNetwork()
 
-	# [g.add_vertex(v) for v in "sopqrt"]
-
-	# g.add_edge('s','o',3)
-	# g.add_edge('s','p',3)
-	# g.add_edge('o','p',2)
-	# g.add_edge('o','q',3)
-	# g.add_edge('p','r',2)
-	# g.add_edge('r','t',3)
-	# g.add_edge('q','r',4)
-	# g.add_edge('q','t',2)
-
-	########## EX - 2 ################
-	# [g.add_vertex(v) for v in "s12345t"]
-	# g.add_edge('s','1',5, 0)
-	# g.add_edge('1','2',7, 1)
-	# g.add_edge('1','3',7, 5)
-	# g.add_edge('2','3',2,-2)
-	# g.add_edge('2','4',3, 8)
-	# g.add_edge('3','4',3,-3)
-	# g.add_edge('3','5',2, 4)
-	# g.add_edge('4','t',3, 0)
-	# g.add_edge('5','t',2, 0)
-
-	##################################
-	print(g.min_cost_max_flow('s','t'))
-	print(g)
-
-def metric(a,b) :
-	if a == None :
-		if b == None :
-			return 0
+if __name__=="__main__" :
+	"""Test case. Duh!"""
+	def metric(a,b) :
+		if a == None :
+			if b == None :
+				return 0
+			else :
+				return metric(b,a)
+		if b != None :
+			return metric((a[0]-b[0],a[1]-b[1]),None)
 		else :
-			return metric(b,a)
-	if b != None :
-		return metric((a[0]-b[0],a[1]-b[1]),None)
-	else :
-		return -a[0] if (a[0]<0) else a[0]
+			return -a[0] if (a[0]<0) else a[0]
 
-ss = ServerSpace(metric)
+	ss = ServerSpace(metric)
 
-ss.add_servers([None, None, None])
-a = None
-b = (1,0)
-c = (12,0)
-d = (13,0)
+	ss.add_servers([None, None, None])
+	a = None
+	b = (1,0)
+	c = (12,0)
+	d = (13,0)
 
-print(ss.process_requests([a,b,c,d,c,d,a,b,a,b]))
+	print(ss.process_requests([a,b,c,d,c,d,a,b,a,b]))
---
+++ WorkFunction.py
@@ -0,0 +1,11 @@
+import NetworkFlow
+
+class WorkFunction(object):
+	"""Class stores the table values for the dynamic 
+	programming algorithm"""
+	def __init__(self):
+		self.arg = arg
+		self.table = []
+
+	def value():
+		return None
